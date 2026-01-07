d49880f680c5bd33c7d5ad22a4bc2806d6e3409c
Wally4S <wally.contatos@gmail.com>
2026-01-06 22:25:47 -0300

    fix loja page client component

--- app/loja/page.tsx
+++ app/loja/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 
-import { Suspense } from "react";
 import Loja from "./loja";
 
-export const dynamic = "force-dynamic";
-
 export default function Page() {
-  return (
-    <Suspense fallback={<div className="p-10 text-white">Carregando loja...</div>}>
-      <Loja />
-    </Suspense>
-  );
+  return <Loja />;
 }
